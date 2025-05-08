32926eb3e37bfb3940f8a0c5174d2c1ecad8f68e
Nikolay Khadzakos <hadzakos@gmail.com>
2025-05-08 15:27:46 +0300

    initial commit

---
+++ file-storing-service/src/test/java/hse/filestoringservice/FileStoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.filestoringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
